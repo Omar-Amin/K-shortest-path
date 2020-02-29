2fdd2f0a854306ce15c84df9c31165653cb54602
Jeppe <jeppe1000@live.dk>
2020-02-29 21:30:47 +0100

    New class xd

--- src/SBTInterface.java
+++ src/SBTInterface.java
@@ -1,4 +1,4 @@
-public interface SBT {
+public interface SBTInterface {
     public void createQueue();
     public void addToQueue(); //Add node to queue
     public int weightOfEdge();
---
+++ src/ShortestPath.java
@@ -0,0 +1,9 @@
+public class ShortestPath {
+    public ShortestPath(Hypergraph h, Vertex source){
+        SBT(h);
+    }
+    //Hypergraph er det samme som hyperpath
+    private Hypergraph SBT(Hypergraph h){
+        return h;
+    }
+}
