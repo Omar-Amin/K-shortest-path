625ae8efca5bbb5b49770fd3f8f710f86de57327
Omar Amin <omar_75@hotmail.dk>
2020-03-21 15:42:54 +0100

    Added a new graph which we can test our backbranching (from rojin) and also added small setup for our algorithm

---
+++ src/KShortestPath.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class KShortestPath {
+
+    public KShortestPath(Hypergraph H, Vertex s, Vertex t,int K){
+
+    }
+
+    private ArrayList<Hypergraph> backBranching(Hypergraph hypergraph,ArrayList<Edge> hyperpath){
+        ArrayList<Hypergraph> setOfHypergraphs = new ArrayList<>();
+
+        return setOfHypergraphs;
+    }
+}
