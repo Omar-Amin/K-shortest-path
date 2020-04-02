64e6580421a03c7df9a77905d681dabd3f049416
Lasse Sørensen <33939871+lass7965@users.noreply.github.com>
2020-04-02 16:35:28 +0200

    minPQ should be fully working

--- src/minPQ.java
+++ src/minPQ.java
@@ -1,38 +1,48 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class minPQ {
     ArrayList<int[]> heap;
+    HashMap<Integer, Integer> index;
     int size;
 
     public minPQ(){
         heap = new ArrayList<>();
+        index = new HashMap<>();
         this.size = 0;
     }
 
     public void minHeapify(int i){
-        if (size == 0) return;
+        if (this.heap.size() == 0) return;
         int smallest = i;
         int lChild = i*2+1;
         int rChild = i*2+2;
         if (lChild <this.size && heap.get(lChild)[1] < heap.get(i)[1]) smallest = lChild;
         if (rChild <this.size && heap.get(rChild)[1] < heap.get(smallest)[1]) smallest = rChild;
         if (smallest != i){
-            int[] tempObj = heap.get(i);
-            heap.set(i,heap.get(smallest));
-            heap.set(smallest,tempObj);
+            exchange(i,smallest);
             minHeapify(smallest);
         }
     }
 
+    public boolean decreaseValue(int id, int value){
+        int i = index.get(id);
+        heap.get(i)[1] = value;
+        int parent = (int) Math.floor((i-1)/2);
+        while (i > 0 && heap.get(parent)[1] > heap.get(i)[1]){
+            exchange(parent,i);
+            i = parent;
+        }
+        return true;
+    }
+
     public boolean insert(int[] obj){
-        int[] tempObj;
         int i = this.size++;
         heap.add(obj);
+        index.put(obj[0],i);
         int parentI = (int) Math.floor((i-1)/2);
         while(i > 0 && heap.get(parentI)[1] > heap.get(i)[1]) {
-            tempObj = heap.get(parentI);
-            heap.set(parentI, heap.get(i));
-            heap.set(i, tempObj);
+            exchange(i,parentI);
             i = parentI;
             parentI = (int) Math.floor((i - 1) / 2);
         }
@@ -58,6 +68,15 @@ public class minPQ {
             if(vertex[0] == obj) return true;
         }
         return false;
+    }
+
+    public void exchange(int swap1, int swap2){
+        int[] swap1Obj = heap.get(swap1);
+        int[] swap2Obj = heap.get(swap2);
+        heap.set(swap1, swap2Obj);
+        index.put(swap1Obj[0],swap1);
+        heap.set(swap2, swap1Obj);
+        index.put(swap2Obj[0],swap2);
     }
 
     public void checkTree(){
