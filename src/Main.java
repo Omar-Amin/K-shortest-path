b0a934fcfd6f3ec4bc8183b6f33b0c2afde9cfa6
Lasse Sørensen <33939871+lass7965@users.noreply.github.com>
2020-04-02 14:50:52 +0200

    WeightFunctions

--- src/Main.java
+++ src/Main.java
@@ -13,17 +13,11 @@ public class Main {
                 { 0, 0, 0, 0, 0, 1, 0, 0,-1, 0},
                 { 0, 0, 0, 0, 0, 0, 1, 1, 1,-1},
                 { 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}};
-        int[] edgePrice = {1,2,3,4,5,6,7,8,9,10};
-        Graph g = new Graph(hypergraph,edgePrice);
-        for (int i = 0; i < g.edgeLookup.length; i++) {
-            System.out.println("Edge "+i+":");
-            System.out.println("Tail: " +Arrays.toString(g.tail(i)));
-            System.out.println("Head: " + g.head(i)+"\n");
-        }
-         for (int i = 0; i < g.vertexLookup.length; i++) {
-            System.out.println("Vertex " + i + ":");
-            System.out.println("Forward Star: "+Arrays.toString(g.FS(i)));
-            System.out.println("Back Star: "+Arrays.toString(g.BS(i))+"\n");
-        }
+        int[] edgePrice = {1,1,1,1,1,1,1,1,1,1};
+        Graph g = new Graph();
+        g.matrixInput(hypergraph,edgePrice);
+        SBT sbt = new SBT(g,function.sum);
+        int[] skip = new int[0];
+        System.out.println(sbt.run(0, 8,skip));
     }
 }
--- src/WeightingFunctions.java
+++ src/WeightingFunctions.java
@@ -1,22 +1,50 @@
+enum function{
+    sum,
+    distance,
+    min
+}
+
 public class WeightingFunctions {
     Graph graph;
-    public WeightingFunctions(Graph graph){
+    function functionType;
+
+    public WeightingFunctions(Graph graph,function toUse){
         this.graph = graph;
+        this.functionType = toUse;
     }
 
-    
-
-    public void valueFunction(){
-        for (int i = 0; i < graph.vertexLookup.length; i++) {
-            int vertex = graph.vertexTable[i];
-
+    public int run(int edge){
+        if(functionType == function.sum){
+            return sumFunction(edge);
+        } else if (functionType == function.distance) {
+            return distanceFunction(edge);
+        } else if (functionType == function.min){
+            return minFunction(edge);
         }
-        return;
+        return -1;
     }
-    public void distanceFunction(){
-        return;
+
+    private int sumFunction(int edge){
+        int sum = graph.getEdgeCost(edge);
+        for (int vertex: graph.tail(edge)) {
+            sum += graph.getVertexCost(vertex);
+        }
+        return sum;
     }
-    public void minFunction(Graph g){
-        return;
+    private int distanceFunction(int edge){
+        int max = -1;
+        for (int vertex: graph.tail(edge)) {
+            int vertexPrice = graph.getVertexCost(vertex);
+            if(max < vertexPrice || max == -1) max = vertexPrice;
+        }
+        return max + graph.getEdgeCost(edge);
+    }
+    private int minFunction(int edge){
+        int min = -1;
+        for (int vertex: graph.tail(edge)) {
+            int vertexPrice = graph.getVertexCost(vertex);
+            if(min > vertexPrice || min == -1) min = vertexPrice;
+        }
+        return min + graph.getEdgeCost(edge);
     }
 }
